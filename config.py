90fc85f7e90e945c143ed02e26cf3f5fbc908750
tangbearrrr <rh.sarawasee@gmail.com>
2021-11-20 03:09:18 +0700

    fix security issue

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    consumer_key: str = "3M3s8eZpwTyUxhIaEpsfwlO4w"
-    consumer_secret: str = "D827LCm6B5fJ2DY4KvydOwDG7cdqeKqTSH8Ai9bQtMI6VN3vMs"
-    access_token: str = "3164075700-UMmOwzGrPuzX5DkfUBZtXIg3f8DbcRlf77DnhvI"
-    access_token_secret: str = "pOufkDfSvTANFmMputocPSMskeEYK7hIwYGdqVjTQR2xl"
+    consumer_key: str = ""
+    consumer_secret: str = ""
+    access_token: str = ""
+    access_token_secret: str = ""
     items_limit: int = 10
 
 settings = Settings()
